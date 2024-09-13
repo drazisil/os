cb21e6636f305bc31266efd9bade3a733fc5cca0
Molly Draven <drazi@duck.com>
2024-09-12 20:16:08 -0400

    Refactor boot.asm to use 16-bit mode and optimize code

--- boot.asm
+++ boot.asm
@@ -1,39 +1,37 @@
+; Bootloader
+; 16-bit real mode
 [bits 16]
-section .ResetSeg
-[org 0x7C00]
-;[org 0]
+
+; The BIOS loads the boot sector into memory at 0000:7C00h
+[org 0x7C00]                
+
+; Jump to the start of the bootloader
 jmp Begin
 
 Begin:
-    ; mov ax, 0x07C0
-    ; mov ds, ax
-    ; mov es, ax
-    ; mov ss, ax
-    ; mov sp, 0x7C00
+    mov si, msg                 ; Load the address of the message into SI
+    call PrintString            ; Print the message
 
-    mov si, msg
-    call PrintString
-
-    jmp $
+    jmp $                       ; Infinite loop    
 
 PrintCharacter:
-    mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x07
-    int 0x10
+    mov ah, 0x0E                ; Function OEh Write Character in TTY Mode
+    mov bh, 0x00                ; Page Number
+    mov bl, 0x07                ; Text Attribute
+    int 0x10                    ; Call Video Interrupt
     ret
 
 PrintString:
     next_character:
-        lodsb
-        or al, al
-        jz done
-        call PrintCharacter
-        jmp next_character
+        lodsb                   ; Load byte at address DS:SI into AL
+        or al, al               ; Check if AL is zero
+        jz done                 ; If zero, jump to done
+        call PrintCharacter     ; Print the character in AL
+        jmp next_character      ; Repeat for next character
     done:
         ret
 
-msg db 'Hello, World!', 0
+msg db 'Hello, World!', 0x0D, 0x0A, 0
 
 times 510-($-$$) db 0
 
